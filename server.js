e4918da61293b61026e846bf35e9c04b8683c002
Aleksandr <shoppingplaneta@gmail.com>
2021-01-24 15:01:17 +0200

    deploy ti herroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files from the dist directory
-app.use(express.static(path.join('./dist/angular-basic')));
+app.use(express.static(path.join('./dist/angular-basic-httpclient')));
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', {root: 'dist/angular-basic'});
+  res.sendFile('index.html', {root: 'dist/angular-basic-httpclient'});
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
